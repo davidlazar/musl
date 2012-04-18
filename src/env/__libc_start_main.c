4ee7a545f0f871aa60534fb3d13b919a45f0da4b
David Lazar <lazar6@illinois.edu>
2012-04-17 23:39:50 -0500

    Split libc_start_main into three "unoverloaded" functions.

--- src/env/__libc_start_main.c
+++ src/env/__libc_start_main.c
@@ -2,7 +2,53 @@
 
 void __init_security(size_t *);
 
-int __libc_start_main(
+int __libc_start_main_0(
+	int (*main)(void), int argc, char **argv,
+	int (*init)(int, char **, char **), void (*fini)(void),
+	void (*ldso_fini)(void))
+{
+	char **envp = argv+argc+1, **auxv = envp;
+
+	__environ = envp;
+//	do auxv++; while (*auxv);
+//	libc.auxv = (void *)++auxv;
+	libc.ldso_fini = ldso_fini;
+	libc.fini = fini;
+
+//	__init_security((void *)auxv);
+
+	/* Execute constructors (static) linked into the application */
+	if (init) init(argc, argv, envp);
+
+	/* Pass control to to application */
+	exit(main());
+	return 0;
+}
+
+int __libc_start_main_2(
+	int (*main)(int, char **), int argc, char **argv,
+	int (*init)(int, char **, char **), void (*fini)(void),
+	void (*ldso_fini)(void))
+{
+	char **envp = argv+argc+1, **auxv = envp;
+
+	__environ = envp;
+	do auxv++; while (*auxv);
+	libc.auxv = (void *)++auxv;
+	libc.ldso_fini = ldso_fini;
+	libc.fini = fini;
+
+	__init_security((void *)auxv);
+
+	/* Execute constructors (static) linked into the application */
+	if (init) init(argc, argv, envp);
+
+	/* Pass control to to application */
+	exit(main(argc, argv));
+	return 0;
+}
+
+int __libc_start_main_3(
 	int (*main)(int, char **, char **), int argc, char **argv,
 	int (*init)(int, char **, char **), void (*fini)(void),
 	void (*ldso_fini)(void))
